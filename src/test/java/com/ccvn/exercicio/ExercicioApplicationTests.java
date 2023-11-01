2f894ce7a85ccaedaff19e6d519d4730d34de6fa
Carolina Carvalho Vila Nova <you@example.com>
2023-11-01 16:20:07 -0300

    first commit

---
+++ src/test/java/com/ccvn/exercicio/ExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccvn.exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
